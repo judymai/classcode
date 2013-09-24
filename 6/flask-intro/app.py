f0c49a6ce71e0e16a4c250a88dba64c37357feca
Mike Zamansky <zamansky@gmail.com>
2013-09-24 12:30:16 -0400

    Z

---
+++ 6/flask-intro/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import render_template
+
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route("/t1")
+@app.route("/t1/<n>")
+def t1(n="Moe"):
+    luckynum=random.randrange(0,100)
+    return render_template("index.html",name=n,
+                           num=luckynum)
+
+@app.route("/lucky")
+def lucky():
+    luckynum = random.randrange(0,100)
+    template="""
+    <h1>This is the lucky number page</h1>
+    Your lucky number is %i!
+    """
+    page = template%(luckynum)
+    
+    return page
+
+@app.route("/who")
+@app.route("/who/<name>")
+def who(name="thedefaultname"):
+    page = """
+    this is a triple quoted string
+    that spans multiple lines
+    """
+    page=page+"<br>"+name
+    return page
+
+@app.route("/about")
+def about():
+    return "<h1>This is the about page</h1>"
+
+if __name__=="__main__":
+    app.debug=True 
+    # 0.0.0.0 means listen on all interfaces
+    app.run(host="0.0.0.0",port=5005)
+
+    
