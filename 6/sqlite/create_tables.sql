820d73b9c869eb0cae368c54753b3b5b74164fc1
Mike Zamansky <zamansky@gmail.com>
2013-10-10 12:34:03 -0400

    Z

---
+++ 6/sqlite/create_tables.sql
@@ -0,0 +1,4 @@
+create table students(id INTEGER, last TEXT, first TEXT);
+create table grades(id INTEGER, class TEXT, grade INTEGER);
+create table classes(class text, description text);
+create table teachers(last text, first text, class text);
