706aed98d80e74125692c461b672b5297c06504d
Mike Zamansky <zamansky@gmail.com>
2013-12-03 13:15:49 -0500

    Updated javascript files

--- 7/javascript/script.js
+++ 7/javascript/script.js
@@ -14,8 +14,26 @@ var f = function (a,b) {
     return a+b;
 }
 
-var stripe = function(q) {
-    var items = document.querySelectorAll(q);
+
+var redback=function(e) {
+    this.classList.toggle('red');
+}
+var green=function(e) {
+    this.classList.toggle('green');
+}
+
+
+var red=function(e) {
+    console.log(e);
+    var items = document.querySelectorAll('li');
+    for (var i = 0; i < items.length; i++) {
+	    items[i].classList.toggle('red');
+	}
+}
+
+var stripe = function(e) {
+    console.log(e);
+    var items = document.querySelectorAll('li');
     for (var i = 0; i < items.length; i++) {
 	if (i%2==0)
 	    items[i].classList.add('red');
@@ -24,3 +42,15 @@ var stripe = function(q) {
 	}
 }
 
+ 
+// var elt=document.getElementById('b1');
+//elt.addEventListener('click',red);
+//var elt=document.getElementById('b2');
+//elt.addEventListener('click',red);
+
+var elts = document.querySelectorAll('li');
+for (var i=0;i<elts.length;i++) {
+    elts[i].addEventListener('click',redback);
+    elts[i].addEventListener('mouseover',green);
+    elts[i].addEventListener('mouseout',green);
+}
