5c701b8cd316b03079407f758cddf4e5dd94d401
Mike Zamansky <zamansky@gmail.com>
2014-01-23 13:10:39 -0500

    Z

--- 7/ajax2/static/main.js
+++ 7/ajax2/static/main.js
@@ -62,7 +62,63 @@ var d3=function() {
     var f2=$.ajax({url:'/delay/3'});
     
     $.when(f1,f2).then(function (a,b) {
-	console.log(a[0]+b[0]);
+	console.log(a+" : "+b);
     });
 
+}
+
+
+var add = function() {
+    return arguments[0]+arguments[1];
+}
+
+var addAll = function() {
+    var sum=0;
+    for (var i=0;i<arguments.length;i++) {
+	sum = sum + arguments[i];
+    }
+    return sum;
+}
+
+
+var printArguments = function () {
+    console.log(arguments);
+}
+
+var dArguments=function() {
+    var f1=$.ajax({url:'/delay/8'});
+    var f2=$.ajax({url:'/delay/3'});
+    var f3=$.ajax({url:'/delay/4'});
+    
+    $.when(f1,f2,f3).then(function () {
+	var r=0;
+	for (var i=0;i<arguments.length;i++) {
+	    r = r + parseInt(arguments[i][0]);
+	    console.log(arguments[i]);
+	}
+	console.log(r);
+    });
+
+}
+
+var dArgumentsApply = function() {
+    
+    var calls = [1,2,5,1,9,3,2,6];
+    
+    var defs=[];
+    for (var i=0;i<calls.length;i++) {
+	defs.push($.ajax({
+	    url:"/delay/"+calls[i]
+	}));
+    }
+    console.log(calls);
+    console.log(defs);
+    $.when.apply(this,defs).then(function () {
+	var r=0;
+	for (var i=0;i<arguments.length;i++) {
+	    r = r + parseInt(arguments[i][0]);
+	    console.log(arguments[i]);
+	}
+	console.log(r);
+    });
 }
