8d2efac9a214f80a086ecdcfeff9e387788fdcf2
Mike Zamansky <zamansky@gmail.com>
2014-01-23 12:19:08 -0500

    Z

--- 6/ajax2/static/main.js
+++ 6/ajax2/static/main.js
@@ -62,10 +62,60 @@ var d3=function() {
     var f1=$.ajax({url:'delay/8'})
     var f2=$.ajax({url:'delay/3'})
 
-    $.when(f1,f2).then(function(a,b) {console.log(a[0]+b[0]);});
+    $.when(f1,f2).then(function(a,b) {console.log(a+" : "+b);});
     console.log("After the ajax call");
 }
 
+
+var add = function(a,b) {
+    console.log(arguments);
+    if (!b) {
+    return a;
+    }
+    
+   return a+b;
+}
+
+
+var printParams = function() {
+    console.log(arguments);
+}
+
+var dArguments=function() {
+
+
+    var f1=$.ajax({url:'delay/8'})
+    var f2=$.ajax({url:'delay/3'})
+    var f3=$.ajax({url:'delay/4'})
+
+    $.when(f1,f2,f3).then(function() {
+	console.log(arguments);
+	console.log(arguments[0][0]+arguments[1][0]);
+	for (var i = 0; i < arguments.length;i++ ){
+	    console.log(arguments[i]);
+	}
+    });
+    console.log("After the ajax call");
+}
+
+dArgumentsApply = function() {
+    
+    var calls = [1,5,4,6,8,2,10,4];
+    
+    var defs=[];
+    for (var i=0;i<calls.length;i++){
+	defs.push( $.ajax({url:"/delay/"+calls[i]}));
+    }
+    console.log(defs);
+    $.when.apply(this,defs).then(function() {
+	console.log(arguments);
+	console.log(arguments[0][0]+arguments[1][0]);
+	for (var i = 0; i < arguments.length;i++ ){
+	    console.log(arguments[i]);
+	}
+	});
+				 
+}
 
 
 
